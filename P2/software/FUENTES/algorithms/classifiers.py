ad3df5905364f9c9db4e9330831f89f06730e419
Vladislav Nikolov <vladis890@gmail.com>
2019-04-29 01:22:39 +0200

    Created classifiers.py to test the different algorithms.

---
+++ P2/software/FUENTES/algorithms/classifiers.py
@@ -0,0 +1,188 @@
+import numpy as np
+import time																									# Medir tiempos
+from enum import Enum																				# Enumerados
+from sklearn.metrics import accuracy_score                  # Medir la precision de los resultados de test
+from sklearn.neighbors import KNeighborsClassifier          # Clasificador KNN
+import algorithms.genetics
+import algorithms.metrics
+import algorithms.memetics
+
+class GeneticCross(Enum):
+		BLX = 1
+		AC = 2
+
+class GeneticReplacement(Enum):
+		GENERATIONAL = 1
+		STATIONARY = 2
+
+class MemeticsLocalSearch(Enum):
+		ALL_POPULATION = 1
+		BEST_CHROMOSOMES = 2
+		RANDOM_CHROMOSOMES = 3
+
+def genetic_classifier(train_part, test_part, cross, replacement):
+    """
+    Implementacion de un clasificador 1-NN con la tecnica de los Algoritmos
+    Geneticos para el calculo de los pesos.
+    Ajusta un clasificador 1-NN con pesos, los cuales se obtienen mediante
+    Algoritmos Geneticos para un conjunto de entrenamiento mediante distintos
+    criterios, como por ejemplo con distintos operadores de cruce o con
+    distintos criterios de reemplazamiento.
+    Despues entrena un clasificador 1-NN de sklearn con los valores de
+    entrenamiento ponderados.
+
+    :param train_part: Particiones de entrenamiento con las que se va
+                       a entrenar el clasificador 1-NN.
+    :param test_part: Particiones de prueba con las que se va a comprobar
+                      el ajuste del clasificador.
+    :param cross: Tipo de cruce (BLX-alfa o cruce aritmetico)
+    :param replacement: Tipo de reemplazo (generacional o estacionario)
+    
+    :return Devuelve arrays creados a apartir de las listas de tasas de 
+            clasificacion, tasas de reduccion, agrupaciones y tiempos.
+    """
+
+    # Crear un nuevo clasificador 1-NN de sklearn
+    neigh = KNeighborsClassifier(n_neighbors=1)
+
+    # Listas para guardar los datos (reduccion, tasa de clas. y tiempo)
+    reduction_list = []
+    accuracy_list = []
+    aggregation_list = []
+    time_list = []
+    
+    # Determinar el operador de cruce a utilizar
+    if cross == GeneticCross.BLX:
+    		cross_func = algorithms.genetics.blx_alpha_crossover
+    else:
+    		cross_func = algorithms.genetics.arithmetic_crossover
+    
+    # Determinar la estrategia de reemplazo a utilizar
+    if replacement == GeneticReplacement.GENERATIONAL:
+    		generational = True
+    else:
+    		generational = False
+
+    # Para cada elemento de las listas de particiones de entrenamiento
+    # y prueba, obtener los w, entrenar el modelo y predecir las etiquetas
+    # Comprobar luego la precision del ajuste
+    for train, test in zip(train_part, test_part):
+        # Tiempo antes de lanzar la Busqueda Local
+        t1 = time.time()
+
+        # Calculo de los pesos mediante el Algoritmo Genetico
+        w = algorithms.genetics.genetic_algorithm(train[0], train[1], cross_func, generational)
+
+        # Tiempo despues de terminar la Busqueda Local
+        t2 = time.time()
+        total_time = t2 - t1
+
+        # Calcular los X de entrenamiento aplicando los pesos y entrenar
+        # el modelo con estos valores
+        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
+        # o igual a 0.2
+        weighted_X_train = (train[0] * w)[:, w >= 0.2]
+        neigh.fit(weighted_X_train, train[1])
+
+        # Calcular los X de test aplicando los pesos y obtener las etiquetas
+        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
+        # o igual a 0.2
+        weighted_X_test = (test[0] * w)[:, w >= 0.2]
+        knn_labels = neigh.predict(weighted_X_test)
+
+        # Obtener tasa de aciertos, reduccion y agrupacion de ambos
+        accuracy = accuracy_score(test[1], knn_labels)
+        reduction = algorithms.metrics.reduction_rate(w)
+        fit_val = algorithms.metrics.fitness(accuracy, reduction)
+        
+        # Insertar los datos en las listas
+        reduction_list.append(reduction)
+        accuracy_list.append(accuracy)
+        aggregation_list.append(fit_val)
+        time_list.append(total_time)
+
+    return np.array(accuracy_list), np.array(reduction_list), np.array(aggregation_list), np.array(time_list)
+
+
+def memetic_classifier(train_part, test_part, ls_evaluations):
+    """
+    Implementacion de un clasificador 1-NN con la tecnica de los Algoritmos
+    Memeticos para el calculo de los pesos.
+    Ajusta un clasificador 1-NN con pesos, los cuales se obtienen mediante
+    Algoritmos Memeticos para un conjunto de entrenamiento mediante distintos
+    criterios, como por ejemplo la cantidad de cromosomas a evaluar en la busqueda
+    local y si estos tienen que ser los mejores o aleatorios.
+    Despues entrena un clasificador 1-NN de sklearn con los valores de
+    entrenamiento ponderados.
+
+    :param train_part: Particiones de entrenamiento con las que se va
+                       a entrenar el clasificador 1-NN.
+    :param test_part: Particiones de prueba con las que se va a comprobar
+                      el ajuste del clasificador.
+    :param ls_evaluations: Tipo de evaluaciones que se haran en la LS
+    
+    :return Devuelve arrays creados a apartir de las listas de tasas de 
+            clasificacion, tasas de reduccion, agrupaciones y tiempos.
+    """
+
+    # Crear un nuevo clasificador 1-NN de sklearn
+    neigh = KNeighborsClassifier(n_neighbors=1)
+
+    # Listas para guardar los datos (reduccion, tasa de clas. y tiempo)
+    reduction_list = []
+    accuracy_list = []
+    aggregation_list = []
+    time_list = []
+    
+    # Decidir los ratios del algoritmo memetico y si se hace con los mejores
+    if ls_evaluations == MemeticsLocalSearch.ALL_POPULATION:
+    		ls_rate = 1.0
+    		ls_best = False
+    elif ls_evaluations == MemeticsLocalSearch.BEST_CHROMOSMES:
+    		ls_rate = 0.1
+    		ls_best = True
+    else:
+    		ls_rate = 0.1
+    		ls_best = False
+
+    # Para cada elemento de las listas de particiones de entrenamiento
+    # y prueba, obtener los w, entrenar el modelo y predecir las etiquetas
+    # Comprobar luego la precision del ajuste
+    for train, test in zip(train_part, test_part):
+        # Tiempo antes de lanzar la Busqueda Local
+        t1 = time.time()
+
+        # Calculo de los pesos mediante la Busqueda Local
+        w = algorithms.memetics.memetic_algorithm(train[0], train[1], ls_rate, ls_best=ls_best)
+
+        # Tiempo despues de terminar la Busqueda Local
+        t2 = time.time()
+        total_time = t2 - t1
+
+        # Calcular los X de entrenamiento aplicando los pesos y entrenar
+        # el modelo con estos valores
+        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
+        # o igual a 0.2
+        weighted_X_train = (train[0] * w)[:, w >= 0.2]
+        neigh.fit(weighted_X_train, train[1])
+
+        # Calcular los X de test aplicando los pesos y obtener las etiquetas
+        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
+        # o igual a 0.2
+        weighted_X_test = (test[0] * w)[:, w >= 0.2]
+        knn_labels = neigh.predict(weighted_X_test)
+
+        # Obtener tasa de aciertos, reduccion y agrupacion de ambos
+        accuracy = accuracy_score(test[1], knn_labels)
+        reduction = algorithms.metrics.reduction_rate(w)
+        fit_val = algorithms.metrics.fitness(accuracy, reduction)
+        
+        # Insertar los datos en las listas
+        reduction_list.append(reduction)
+        accuracy_list.append(accuracy)
+        aggregation_list.append(fit_val)
+        time_list.append(total_time)
+
+    return np.array(accuracy_list), np.array(reduction_list), np.array(aggregation_list), np.array(time_list)
+
+
--- P2/software/FUENTES/practica2.py
+++ P2/software/FUENTES/practica2.py
@@ -1,156 +1,14 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score                  # Medir la precision de los resultados de test
-from sklearn.neighbors import KNeighborsClassifier          # Clasificador KNN
-import time                                                 # Medir el tiempo
 import sys                                                  # Argumentos de la linea de comandos
-
-try:
-    from pykdtree.kdtree import KDTree                      # Implementacion paralela de KDTree
-except ImportError:
-    from scipy.spatial import cKDTree as KDTree             # En caso de fallar, importar cKDTree como KDTree
-
 import algorithms.kfold
-import algorithms.genetics
-import algorithms.metrics
-import algorithms.memetics
-
-def genetic_classifier(train_part, test_part):
-    """
-    Implementacion de un clasificador 1-NN con la tecnica de la Busqueda Local
-    para el calculo de los pesos.
-    Ajusta un clasificador 1-NN con pesos, los cuales se obtienen mediante
-    una Busqueda Local para un conjunto de entrenamiento. Despues entrena un 
-    clasificador 1-NN de sklearn con los valores de entrenamiento ponderados.
-
-    :param train_part: Particiones de entrenamiento con las que se va
-                       a entrenar el clasificador 1-NN.
-    :param test_part: Particiones de prueba con las que se va a comprobar
-                      el ajuste del clasificador.
-    :return Devuelve arrays creados a apartir de las listas de tasas de 
-            clasificacion, tasas de reduccion, agrupaciones y tiempos.
-    """
-
-    # Crear un nuevo clasificador 1-NN de sklearn
-    neigh = KNeighborsClassifier(n_neighbors=1)
-
-    # Listas para guardar los datos (reduccion, tasa de clas. y tiempo)
-    reduction_list = []
-    accuracy_list = []
-    aggregation_list = []
-    time_list = []
-    
-    cross_func = algorithms.genetics.blx_alpha_crossover
-
-    # Para cada elemento de las listas de particiones de entrenamiento
-    # y prueba, obtener los w, entrenar el modelo y predecir las etiquetas
-    # Comprobar luego la precision del ajuste
-    for train, test in zip(train_part, test_part):
-        # Tiempo antes de lanzar la Busqueda Local
-        t1 = time.time()
-
-        # Calculo de los pesos mediante la Busqueda Local
-        w = algorithms.genetics.genetic_algorithm(train[0], train[1], cross_func, generational=True)
-
-        # Tiempo despues de terminar la Busqueda Local
-        t2 = time.time()
-        total_time = t2 - t1
-
-        # Calcular los X de entrenamiento aplicando los pesos y entrenar
-        # el modelo con estos valores
-        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
-        # o igual a 0.2
-        weighted_X_train = (train[0] * w)[:, w >= 0.2]
-        neigh.fit(weighted_X_train, train[1])
-
-        # Calcular los X de test aplicando los pesos y obtener las etiquetas
-        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
-        # o igual a 0.2
-        weighted_X_test = (test[0] * w)[:, w >= 0.2]
-        knn_labels = neigh.predict(weighted_X_test)
-
-        # Obtener tasa de aciertos, reduccion y agrupacion de ambos
-        accuracy = accuracy_score(test[1], knn_labels)
-        reduction = algorithms.metrics.reduction_rate(w)
-        fit_val = algorithms.metrics.fitness(accuracy, reduction)
-        
-        # Insertar los datos en las listas
-        reduction_list.append(reduction)
-        accuracy_list.append(accuracy)
-        aggregation_list.append(fit_val)
-        time_list.append(total_time)
-
-    return np.array(accuracy_list), np.array(reduction_list), np.array(aggregation_list), np.array(time_list)
-
-
-def memetic_classifier(train_part, test_part):
-    """
-    Implementacion de un clasificador 1-NN con la tecnica de la Busqueda Local
-    para el calculo de los pesos.
-    Ajusta un clasificador 1-NN con pesos, los cuales se obtienen mediante
-    una Busqueda Local para un conjunto de entrenamiento. Despues entrena un 
-    clasificador 1-NN de sklearn con los valores de entrenamiento ponderados.
-
-    :param train_part: Particiones de entrenamiento con las que se va
-                       a entrenar el clasificador 1-NN.
-    :param test_part: Particiones de prueba con las que se va a comprobar
-                      el ajuste del clasificador.
-    :return Devuelve arrays creados a apartir de las listas de tasas de 
-            clasificacion, tasas de reduccion, agrupaciones y tiempos.
-    """
-
-    # Crear un nuevo clasificador 1-NN de sklearn
-    neigh = KNeighborsClassifier(n_neighbors=1)
-
-    # Listas para guardar los datos (reduccion, tasa de clas. y tiempo)
-    reduction_list = []
-    accuracy_list = []
-    aggregation_list = []
-    time_list = []
-
-    # Para cada elemento de las listas de particiones de entrenamiento
-    # y prueba, obtener los w, entrenar el modelo y predecir las etiquetas
-    # Comprobar luego la precision del ajuste
-    for train, test in zip(train_part, test_part):
-        # Tiempo antes de lanzar la Busqueda Local
-        t1 = time.time()
-
-        # Calculo de los pesos mediante la Busqueda Local
-        w = algorithms.memetics.memetic_algorithm(train[0], train[1], 0.1, ls_best=True)
-
-        # Tiempo despues de terminar la Busqueda Local
-        t2 = time.time()
-        total_time = t2 - t1
-
-        # Calcular los X de entrenamiento aplicando los pesos y entrenar
-        # el modelo con estos valores
-        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
-        # o igual a 0.2
-        weighted_X_train = (train[0] * w)[:, w >= 0.2]
-        neigh.fit(weighted_X_train, train[1])
-
-        # Calcular los X de test aplicando los pesos y obtener las etiquetas
-        # Se seleccionan solo aquellas caracteristicas cuyo w_i sea superior
-        # o igual a 0.2
-        weighted_X_test = (test[0] * w)[:, w >= 0.2]
-        knn_labels = neigh.predict(weighted_X_test)
-
-        # Obtener tasa de aciertos, reduccion y agrupacion de ambos
-        accuracy = accuracy_score(test[1], knn_labels)
-        reduction = algorithms.metrics.reduction_rate(w)
-        fit_val = algorithms.metrics.fitness(accuracy, reduction)
-        
-        # Insertar los datos en las listas
-        reduction_list.append(reduction)
-        accuracy_list.append(accuracy)
-        aggregation_list.append(fit_val)
-        time_list.append(total_time)
-
-    return np.array(accuracy_list), np.array(reduction_list), np.array(aggregation_list), np.array(time_list)
+import algorithms.classifiers
 
 
 
-csv_file = '../BIN/texture.csv'
+
+
+csv_file = '../BIN/colposcopy.csv'
 
 # Cargar el archivo csv que contiene los datos y obtener la muestra
 df = pd.read_csv(csv_file)
@@ -166,9 +24,10 @@ sample_y = sample[:, -1].reshape(-1,)
 # La segunda contiene 5 particiones de test con sus (x, y)
 train_part, test_part = algorithms.kfold.stratify_sample(sample_x, sample_y)
 
+# Establecer la semilla aleatoria
 np.random.seed(8912374)
 
-class_rate, red_rate, aggrupation, times = genetic_classifier(train_part, test_part)
+class_rate, red_rate, aggrupation, times = memetic_classifier(train_part, test_part)
 
 
 print('Tiempo total: {}'.format(times.sum()))
